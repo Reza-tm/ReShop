92f08d4bc0d5ab73c4235568fe11a6b1d324cb0f
Reza-tm <rezatm331@gmail.com>
2021-12-15 02:14:16 +0330

    add animation to sign in and make sign up page

---
+++ src/components/SignInPage/SigninForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import SinginInput from "./SinginInput";
+import { IoMail } from "react-icons/io5";
+import { IoMdKey } from "react-icons/io";
+import { signInWithGoogle } from "../Firebase/GoogleSignin";
+import { signinWithGithub } from "../Firebase/GithubSignin";
+import { signinWithEmailPass } from "../Firebase/EmailPassSignin";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { AnimatePresence, motion } from "framer-motion";
+
+const SigninForm = ({ setter }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      pass: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Please enter a valid email").required("Required"),
+      pass: Yup.string().min(6, "Must be 6 characters or more").required("Required"),
+    }),
+    onSubmit: (values) => {
+      signinWithEmailPass(values);
+    },
+  });
+
+  return (
+    <form
+      onSubmit={formik.handleSubmit}
+      className="rounded-2xl duration-300 z-10 relative text-center md:text-left space-y-5 flex flex-col justify-center"
+    >
+      <h2 className="font-nunito text-white text-2xl font-bold">Sign in and enjoy app</h2>
+      <p className="font-nunito text-white/50">Welcome Back !</p>
+      <SinginInput
+        id="email"
+        value={formik.values.email}
+        placeholder="Email"
+        {...formik.getFieldProps("email")}
+        icon={<IoMail size={25} color="white" />}
+        error={formik.errors.email}
+        touch={formik.touched.email}
+      />
+      <SinginInput
+        value={formik.values.pass}
+        placeholder="Password"
+        icon={<IoMdKey size={25} color="white" />}
+        {...formik.getFieldProps("pass")}
+        id="pass"
+        error={formik.errors.pass}
+        touch={formik.touched.pass}
+      />
+
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        className="w-80 flex-shrink-0 h-12 mx-auto md:mx-0 rounded-md text-white font-semibold active:bg-blue-900 bg-blue-700 hover:bg-blue-800 transition-colors duration-300"
+      >
+        Create my account
+      </motion.button>
+      <div className="flex w-80 justify-between mx-auto md:mx-0">
+        <motion.button
+          type="button"
+          whileTap={{ scale: 0.95 }}
+          onClick={() => signInWithGoogle()}
+          className="w-[48%] h-12 mx-auto md:mx-0 rounded-md text-white font-semibold bg-slate-700 active:bg-red-900 hover:bg-red-800 transition-colors duration-300"
+        >
+          Gmail
+        </motion.button>
+        <motion.button
+          type="button"
+          whileTap={{ scale: 0.95 }}
+          onClick={() => signinWithGithub()}
+          className="w-[48%] h-12 mx-auto md:mx-0 rounded-md text-white font-semibold bg-slate-700 active:bg-gray-900 hover:bg-gray-800 transition-colors duration-300"
+        >
+          Github
+        </motion.button>
+      </div>
+      <p className="text-center mx-auto md:mx-0 text-white/50 w-80 text-sm">
+        Wana create account ?{" "}
+        <span onClick={() => setter(false)} className="cursor-pointer text-blue-700">
+          Sign up
+        </span>
+      </p>
+    </form>
+  );
+};
+
+export default SigninForm;
---
+++ src/pages/signin/SigninAndSignup.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import SigninForm from "../../components/SigninPage/SigninForm";
+import SigninImage from "../../assets/images/signin-image.png";
+import { AnimatePresence, motion } from "framer-motion";
+import SignupForm from "../../components/SigninPage/SignupForm";
+
+const Signin = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [isSignIn, setIsSignIn] = useState(false);
+
+  return (
+    <main className=" md:h-screen mx-auto flex md:flex-row flex-col justify-around items-center">
+      <div className="w-1/2 flex justify-center">
+        <div className="sm:w-64  mt-20 md:mt-0 md:w-96 lg:w-[500px]">
+          <img src={SigninImage} />
+        </div>
+      </div>
+      <div className="w-1/2 flex justify-center">
+        <AnimatePresence onExitComplete={() => setIsSignIn(true)}>
+          {isSignUp && (
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              <SignupForm setter={setIsSignUp} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <AnimatePresence onExitComplete={() => setIsSignUp(true)}>
+          {isSignIn && (
+            <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+              <SigninForm setter={setIsSignIn} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </main>
+  );
+};
+
+export default Signin;
