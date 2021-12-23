661d8ef17bf710d53426f03cd29171b751f7b338
Reza-tm <rezatm331@gmail.com>
2021-12-23 08:20:35 +0330

    fix navbar bug to always be center

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,32 +6,39 @@ import getData from "./Header.data.js";
 import { RiVipCrownLine } from "react-icons/ri";
 import CartModal from "../CartModal/CartModal";
 import CartBadge from "../CartBadge/CartBadge";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const data = getData();
   const [cartModalVisibility, setCartModalVisibility] = useState(false);
 
   return (
-    <div className="bg-gray-50/5 items-center w-24 p-3 flex flex-col rounded-r-lg h-80 justify-between sticky centerY">
-      <div className="relative">
+    <div className="bg-gray-50/5 items-center w-24 p-3 flex flex-col rounded-r-lg h-80 justify-between fixed centerY">
+      <motion.div whileHover={{ rotate: "10deg" }} className="relative">
         <RiVipCrownLine color="white" size="40px" />
-      </div>
+      </motion.div>
       <div className="flex flex-col items-center  justify-around h-full mt-2 text-gray-200">
         {data.map(({ icon, to, type }) =>
           type == "link" ? (
-            <Link to={to}>{icon}</Link>
+            <motion.div whileHover={{ scale: 1.2 }}>
+              <Link to={to}>{icon}</Link>
+            </motion.div>
           ) : type == "modal" ? (
-            <div className=" cursor-pointer">
-              <div onClick={() => setCartModalVisibility(!cartModalVisibility)} className="relative">
+            <div>
+              <motion.div
+                onClick={() => setCartModalVisibility(!cartModalVisibility)}
+                whileHover={{ scale: 1.2 }}
+                className=" cursor-pointer relative"
+              >
                 {icon}
                 <CartBadge />
-              </div>
+              </motion.div>
               <CartModal visibility={cartModalVisibility}></CartModal>
             </div>
           ) : (
-            <div onClick={() => signOut(auth)} className="cursor-pointer">
+            <motion.div whileHover={{ scale: 1.2 }} onClick={() => signOut(auth)} className="cursor-pointer">
               {icon}
-            </div>
+            </motion.div>
           )
         )}
       </div>
