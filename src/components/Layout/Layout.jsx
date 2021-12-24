55036bfc330652f21df40295b54f4d16a8d4b333
Reza-tm <rezatm331@gmail.com>
2021-12-25 02:35:49 +0330

    fix a bug in cart modall

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,10 +4,10 @@ import Header from "../Header/Header";
 const Layout = ({ children }) => {
   return (
     <div className="flex items-center justify-center overflow-hidden h-full">
-      <header className="relative flex-shrink-0 h-screen ">
+      <header className="z-10 relative flex-shrink-0 h-screen ">
         <Header />
       </header>
-      <main className="w-full pl-32  pr-5">{children}</main>
+      <main className="z-0 w-full pl-32  pr-5">{children}</main>
     </div>
   );
 };
