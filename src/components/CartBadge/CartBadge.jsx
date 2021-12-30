1e10527403180f5be23ca556b5fc3832302f2fc8
Reza-tm <rezatm331@gmail.com>
2021-12-30 16:53:46 +0330

    add badge numebr

--- src/components/CartBadge/CartBadge.jsx
+++ src/components/CartBadge/CartBadge.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const CartBadge = ({ numOfItems }) => {
+const CartBadge = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   return (
     <div className="absolute -right-4 -top-2 rounded-full w-5 h-5 select-none bg-white">
       <div className="relative w-full  h-full">
-        <p className="centerXY absolute text-xs text-teal-600 font-bold">{numOfItems ? numOfItems : 0}</p>
+        <p className="centerXY absolute text-xs text-teal-600 font-bold">{cartItems.length}</p>
       </div>
     </div>
   );
--- src/pages/mainApp/MainApp.js
+++ src/pages/mainApp/MainApp.js
@@ -39,13 +39,7 @@ const MainApp = () => {
       </AnimatePresence>
       <AnimatePresence>
         {!categories && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            transition={{ delay: 0.2 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="flex flex-wrap"
-          >
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="flex flex-wrap">
             <CategorySkeleton />
             <CategorySkeleton />
             <CategorySkeleton />
