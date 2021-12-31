c26ed6cca1b88eb03d1e36647af40ad7907fdd57
Reza-tm <rezatm331@gmail.com>
2021-12-31 14:59:21 +0330

    fix redux toolkit bug

--- src/services/Redux/store.js
+++ src/services/Redux/store.js
@@ -21,7 +21,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, "user/userVerification"],
       },
     }),
 });
