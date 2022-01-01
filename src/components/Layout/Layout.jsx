a656b9765b98951ee73a1b7cbbffee5f79bcc8a9
Reza-tm <rezatm331@gmail.com>
2022-01-02 01:32:52 +0330

    optimze app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { closeModal } from "../../services/Redux/cart/cartSlice";
 import Header from "../Header/Header";
 
 const Layout = ({ children }) => {
   const dispatch = useDispatch();
+  const modalState = useSelector((state) => state.cart.cartModal);
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center">
       <header className="z-10 w-full lg:w-auto mx-auto relative flex-shrink-0 lg:h-screen ">
         <Header />
       </header>
-      <main onClick={() => dispatch(closeModal())} className="z-0  w-full lg:pl-32 py-4 lg:pr-5 px-5">
+      <main
+        onClick={() => {
+          if (modalState) {
+            dispatch(closeModal());
+          }
+        }}
+        className="z-0  w-full lg:pl-32 py-4 lg:pr-5 px-5"
+      >
         {children}
       </main>
     </div>
   );
 };
 
-export default Layout;
+export default React.memo(Layout);
